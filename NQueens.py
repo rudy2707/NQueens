46660bf7bfbfa9bd84c584794c3594737d1115f3
Axel Fahy <axel@fahy.net>
2015-08-25 10:32:44 +0200

    Add color for the plot.

--- NQueens.py
+++ NQueens.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+import colorama
+
 N = 4
 
 def plot_queens(queens, conflicts=None):
-    for i in range(len(queens)):
-        for j in range(len(queens)):
+    colorama.init()
+
+    # If conflicts is not provided, we create a list with 0.
+    if conflicts is None:
+        conflicts = [0] * N
+    # Print the board.
+    # A queen is represented by a 'x', an empty case by a 'o'.
+    for i in range(N):
+        for j in range(N):
             if i == queens[j]:
-                print("x", end=' ')
+                # If a queen threat another one, the color is red, else green.
+                if conflicts[j]:
+                    print(colorama.Fore.RED + "x" + colorama.Style.RESET_ALL, end=' ')
+                else:
+                    print(colorama.Fore.GREEN + "x" + colorama.Style.RESET_ALL, end=' ')
             else:
                 print("o", end=' ')
         print("")
@@ -19,7 +32,8 @@ def search_conflicts(queens):
 
 def main():
     queens = [1, 2, 3, 2]
-    plot_queens(queens)
+    conflicts = [0, 1, 0, 1]
+    plot_queens(queens, conflicts)
 
 if __name__ == '__main__':
     main()
