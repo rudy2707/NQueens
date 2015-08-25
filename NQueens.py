470e934c2c836de0e0e4d28c629e91210eb28b95
Hohn Rudolf <rudolf.hohn@gmail.com>
2015-08-25 10:33:26 +0200

    search_conflicts and generator of queens done.

--- NQueens.py
+++ NQueens.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+import random
 
-def plot_queens(queens, conflicts=None):
+# Number of queens
+N = 4
+
+# def plot_queens(queens, conflicts=None):
 
 
 def search_conflicts(queens):
+    conflicts = [0] * N
 
+    # find conflicts in diagonals
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                if abs(queens[j] - queens[i]) == abs(j - i):
+                    conflicts[j] = 1
+
+    return conflicts
 
 def main():
-    print("test")
+    # generate the queens
+    queens = random.sample(range(N), N)
+
+    print(search_conflicts(queens))
 
 if __name__ == '__main__':
     main()
