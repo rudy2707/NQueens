8bbbb04f69da918c0293aee9d43046938d8d0482
Axel Fahy <axel@fahy.net>
2015-08-25 10:00:47 +0200

    First version of plot_queens function.

--- NQueens.py
+++ NQueens.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+N = 4
 
 def plot_queens(queens, conflicts=None):
+    for i in range(len(queens)):
+        for j in range(len(queens)):
+            if i == queens[j]:
+                print("x", end=' ')
+            else:
+                print("o", end=' ')
+        print("")
 
 
 def search_conflicts(queens):
+    print("test")
 
 
 def main():
-    print("test")
+    queens = [1, 2, 3, 2]
+    plot_queens(queens)
 
 if __name__ == '__main__':
     main()
