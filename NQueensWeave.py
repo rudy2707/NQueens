6b46081b6cae3154b1a7fb435ffd5241d9cccf6e
Axel Fahy <axel@fahy.net>
2015-08-28 14:16:33 +0200

    Weave implementations, not tested. Do not work with python3

---
+++ NQueensWeave.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import colorama
+import random
+from scipy import weave
+
+# NOT TESTED ! WEAVE NOT AVAILABLE WITH PYTHON3
+
+# Number of queens
+N = 10
+
+def plot_queens(queens, conflicts=None):
+    """Print the board with the queens.
+
+    A queen is represented by a 'x' on the board and an empty case by a 'o'.
+    If there is a conflicts, the 'x' is red, else it's green.
+
+    Keyword arguments:
+    queens      -- the list with the positions of the queens
+    conflicts   -- optional list with the conflicts between the queens
+    """
+
+    colorama.init()
+
+    # If conflicts is not provided, we create a list with 0
+    if conflicts is None:
+        conflicts = [0] * N
+    # Print the board.
+    # A queen is represented by a 'x', an empty case by a 'o'
+    for i in range(N):
+        for j in range(N):
+            if i == queens[j]:
+                # If a queen threat another one, the color is red, else green
+                if conflicts[j]:
+                    print(colorama.Fore.RED + "x" + colorama.Style.RESET_ALL, end=' ')
+                else:
+                    print(colorama.Fore.GREEN + "x" + colorama.Style.RESET_ALL, end=' ')
+            else:
+                print("o", end=' ')
+        print("")
+
+
+def search_conflicts(queens):
+    """Search the conflicts between the queens.
+
+    The positions of the queens are stored in a list.
+    The index represents the column and the value the line number.
+
+    There is no conflict in the column or the line, so we only need to check the diagonals.
+    If the difference of the i and the j position are the same between the queens,
+    they are on the same diagonal.
+
+    Keyword arguments:
+    queens      -- the list with the positions of the queens
+    """
+
+    conflicts = [0] * N
+
+    code = """
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if abs(queens[j] - queens[i] == abs(j - i):
+                conflicts[i] = 1
+                conflicts[j] = 1
+        }
+    }
+    """
+    weave.inline(code, ['queens', 'conflicts'],
+                 type_converters=weave.converters.blitz,
+                 extra_compile_args=['03'])
+
+    return conflicts
+
+def main():
+    """Main function."""
+    # Generate the first position of the queens and print it.
+    queens = random.sample(range(N), N)
+    cnt = 1
+
+    print("First board :")
+    plot_queens(queens, search_conflicts(queens))
+
+    # Look for a solution by generating random boards.
+    while any(search_conflicts(queens)):
+        queens = random.sample(range(N), N)
+        cnt += 1
+
+    print("\nA solution :")
+    plot_queens(queens, search_conflicts(queens))
+    print("\nNumber of tries : ", cnt)
+
+
+if __name__ == '__main__':
+    main()
